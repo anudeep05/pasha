2459d5dd62ccc7f3a8bde14af15d32634e3cd8a5
gundaanudeep <gundaanudeep@HDDLBGKIOSK17.Virtusa.com>
2019-06-13 15:30:31 +0530

    commit1

---
+++ src/main/java/anu/lbg/amazon.java
@@ -0,0 +1,14 @@
+package anu.lbg;
+
+public class amazon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a,b,c;
+a =10;
+b=20;
+c=a-b;
+System.out.println("This result =" +c);
+	}
+
+}
